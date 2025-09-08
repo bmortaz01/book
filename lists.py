9dc913fb8f51ef7a5dc52203e575ad6260299cc9
Behzad Mortazavi <bmorta01@student.ubc.ca>
2025-09-07 17:51:55 -0700

    working on deepcoy of lists chapter

--- lists.py
+++ lists.py
@@ -1,3 +1,5 @@
+import copy
+
 def compare_fun(lista):
     return lista[2]
 x = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
@@ -13,3 +15,8 @@ x.insert(0, 2)
 if 2 in x and x.count(2) == 2: 
     x.remove(2)
     print(x)
+
+a = copy.deepcopy(z)
+a[0][0] = 0
+print(a)
+print(z)
